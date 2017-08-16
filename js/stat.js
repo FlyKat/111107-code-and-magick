1e9666d34eb9f8c0b600786a4a8b3faf29e2e4a2
Ekaterina Davliatchina <flykat@yandex.ru>
2017-08-17 02:01:08 +1000

    refactor

--- js/stat.js
+++ js/stat.js
@@ -1,53 +1,52 @@
 'use strict';
 
+var COLOR_RED = 'rgba(255, 0, 0, 1)';
+var COLOR_WHITE = 'rgb(255, 255, 255)';
+var COLOR_BLACK = 'rgb(0, 0, 0)';
+
+function drawCloud(ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.rect(x, y, width, height);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+}
+
+function typeMessage(ctx, font, color, text, x, y) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(text, x, y);
+}
+
+function getMaxNumber(arr) {
+  return Math.max.apply(null, arr);
+}
+
+function getRandomColorBlue() {
+  return 'rgba(0, 0, 255, ' + (Math.random() * 0.9 + 0.1).toFixed(1) + ')';
+}
+
+
 window.renderStatistics = function (ctx, names, times) {
 
-  function drawCloud (ctx, X, Y, width, height, color) {
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.rect(X, Y, width, height);
-    ctx.closePath();
-    ctx.stroke;
-    ctx.fill();
-  }
-
-  drawCloud(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
-  drawCloud(ctx, 100, 10, 420, 270, 'rgb(255, 255, 255)');
-
-  function typeMessage(ctx, font, color, text, X, Y) {
-    ctx.fillStyle = color;
-    ctx.font = font;
-    ctx.fillText(text, X, Y);
-  }
-
-  typeMessage(ctx, '16px PT Mono', 'rgb(0, 0, 0)', 'Ура, вы победили!', 120, 40);
-  typeMessage(ctx, '16px PT Mono', 'rgb(0, 0, 0)', 'Список результатов:', 120, 60);
-
-  function getMaxNumber(arr) {
-    return Math.max.apply(null, arr)
-  }
-
-  function getRandomNumber() {
-    return +Math.random().toFixed(1);
-  }
-
   function drawHistogram() {
+
     var max = getMaxNumber(times);
-    var histogramHeight = 150;
-    var step = histogramHeight / max;
+    var histogramHeightMax = 150;
+    var step = histogramHeightMax / max;
     var barWidth = 40;
     var indent = 90;
     var initialX = 150;
     var initialY = 250;
+    var timesLength = times.length;
 
-    for(var i = 0; i < times.length; i++) {
+    for (var i = 0; i < timesLength; i++) {
 
-    //names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, getRandomNumber())'
-    //names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, +Math.random().toFixed(1))';
-      names[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, 0.3)';
+      names[i] === 'Вы' ? ctx.fillStyle = COLOR_RED : ctx.fillStyle = getRandomColorBlue();
 
       ctx.fillRect(initialX + indent * i, initialY, barWidth, -step * times[i]);
-      ctx.fillStyle = 'rgb(0, 0, 0)';
+      ctx.fillStyle = COLOR_BLACK;
       ctx.textBaseline = 'top';
       ctx.fillText(names[i], initialX + indent * i, initialY);
       ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - step * times[i] - 20);
@@ -55,6 +54,10 @@ window.renderStatistics = function (ctx, names, times) {
 
   }
 
-  drawHistogram();
+  drawCloud(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+  drawCloud(ctx, 100, 10, 420, 270, COLOR_WHITE);
+  typeMessage(ctx, '16px PT Mono', COLOR_BLACK, 'Ура, вы победили!', 120, 40);
+  typeMessage(ctx, '16px PT Mono', COLOR_BLACK, 'Список результатов:', 120, 60);
+  drawHistogram(ctx, times, names);
 
 };
