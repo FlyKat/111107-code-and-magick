179b6a5b2c9107a0efad2762b5cf38cdb513a5e9
Ekaterina Davliatchina <flykat@yandex.ru>
2017-09-02 03:13:48 +1000

    start task

---
+++ js/dialog.js
@@ -0,0 +1,97 @@
+'use strict';
+
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var setupSubmit = setup.querySelector('.setup-submit');
+  var setupUserName = setup.querySelector('.setup-user-name');
+
+  function popupEscPressHandler(evt) {
+    window.util.isEscEvent(evt, function () {
+      if (evt.target !== setupUserName) {
+        closePopup();
+      }
+    });
+  }
+
+  function openPopup() {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', popupEscPressHandler);
+  }
+
+  function closePopup() {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', popupEscPressHandler);
+  }
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window. util.isEnterEvent(evt, closePopup);
+  });
+
+  setupSubmit.addEventListener('click', function () {
+    if (setupUserName.checkValidity() === true) {
+      closePopup();
+    }
+  });
+
+  setupSubmit.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  /**
+   * Показывает блок похожих персонажей
+   */
+  setup.querySelector('.setup-similar').classList.remove('hidden');
+
+  var dialogHandle = setup.querySelector('.setup-user-pic');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function MouseMoveHandler(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    function MouseUpHandler(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', MouseMoveHandler);
+      document.removeEventListener('mouseup', MouseUpHadler);
+   }
+
+    document.addEventListener('mousemove', MouseMoveHandler);
+    document.addEventListener('mouseup', MouseUpHadler);
+  });
+})();
--- js/setup.js
+++ js/setup.js
@@ -2,58 +2,74 @@
 
 (function () {
   var setup = document.querySelector('.setup');
-  var setupOpen = document.querySelector('.setup-open');
-  var setupClose = setup.querySelector('.setup-close');
-  var setupSubmit = setup.querySelector('.setup-submit');
-  var setupUserName = setup.querySelector('.setup-user-name');
+  var similarListElement = setup.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+  var wizardSetup = setup.querySelector('.setup-wizard');
+  var wizardCoat = wizardSetup.querySelector('.wizard-coat');
+  var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
+  var fireball = setup.querySelector('.setup-fireball-wrap');
+  var WIZARD_PARAMS = window.data.WIZARD_PARAMS;
+  var wizards = window.data.wizards;
 
-  function popupEscPressHandler(evt) {
-    window.util.isEscEvent(evt, function () {
-      if (evt.target !== setupUserName) {
-        closePopup();
-      }
-    });
+  /**
+   * Создаёт DOM-элемент на основе JS-объекта wizard
+   * @param  {object} wizard
+   * @return {type} DOM-элемент (Node)
+   */
+  function renderWizard(wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+    return wizardElement;
   }
 
-  function openPopup() {
-    setup.classList.remove('hidden');
-    document.addEventListener('keydown', popupEscPressHandler);
-  }
+  /**
+   * Заполняет блок DOM-элементами на основе массива JS-объектов
+   * @return {type} DocumentFragment
+   */
+  function renderWizards() {
+    var fragment = document.createDocumentFragment();
+    var count = wizards.length;
 
-  function closePopup() {
-    setup.classList.add('hidden');
-    document.removeEventListener('keydown', popupEscPressHandler);
-  }
-
-  setupOpen.addEventListener('click', function () {
-    openPopup();
-  });
-
-  setupOpen.addEventListener('keydown', function (evt) {
-    window.util.isEnterEvent(evt, openPopup);
-  });
-
-
-  setupClose.addEventListener('click', function () {
-    closePopup();
-  });
-
-  setupClose.addEventListener('keydown', function (evt) {
-    window. util.isEnterEvent(evt, closePopup);
-  });
-
-  setupSubmit.addEventListener('click', function () {
-    if (setupUserName.checkValidity() === true) {
-      closePopup();
+    for (var i = 0; i < count; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
+
+    return fragment;
+  }
+
+  function getWizardElementRandomColor(elem, arr) {
+    elem.style.fill = window.util.getRandomElement(arr);
+  }
+
+
+  function getRandomFireballColor() {
+    fireball.style.background = window.util.getRandomElement(WIZARD_PARAMS.fireballColors);
+  }
+
+
+  wizardCoat.addEventListener('click', function () {
+    getWizardElementRandomColor(wizardCoat, WIZARD_PARAMS.coatColors);
   });
 
-  setupSubmit.addEventListener('keydown', function (evt) {
-    window.util.isEnterEvent(evt, closePopup);
+  wizardEyes.addEventListener('click', function () {
+    getWizardElementRandomColor(wizardEyes, WIZARD_PARAMS.eyesColors);
+  });
+
+  fireball.addEventListener('click', function () {
+    getRandomFireballColor();
   });
 
   /**
-   * Показывает блок похожих персонажей
+   *Отрисовывает похожих персонажей во временном блоке DocumentFragment
    */
-  setup.querySelector('.setup-similar').classList.remove('hidden');
+  renderWizards();
+
+  /**
+   * Отрисовывает похожих персонажей (заполняет блок similarListElement DOM-элементами из DocumentFragment)
+   */
+  similarListElement.appendChild(renderWizards());
 })();
